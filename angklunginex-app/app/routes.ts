238b9a7f9f45def86a7a45750aea06fa0114345c
Solthera <farelfrr09@gmail.com>
2026-09-02 14:26:39 +0700

    feat: edit addtional

--- angklunginex-app/app/routes.ts
+++ angklunginex-app/app/routes.ts
@@ -3,5 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("free-play", "features/free-play/free-play-page.tsx"),
+  route("angklunginex", "features/angklunginex/angklunginex.tsx"),
+
   route("*", "routes/splat.tsx"),
 ] satisfies RouteConfig;
